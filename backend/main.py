5a2426e9477b37d7e51547e29e4e37d63432eabd
José Antonio Hernández Humanes <jherhum1702@g.educaand.es>
2026-04-16 23:25:15 +0200

    refactor(core): update exceptions and main entry point for auth

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,13 @@
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from db.database import create_db_and_tables
+from backend.db.database import create_db_and_tables
 
-from api.routes.auth import router as auth_router
-from api.routes.queue import router as queue_router
-from api.routes.search import router as search_router
-from api.routes.session import router as session_router
-from api.routes.user import router as user_router
-from core.config import settings
+from backend.api.auth.router import router as auth_router
+from backend.api.queue.router import router as queue_router
+from backend.api.search.router import router as search_router
+from backend.api.session.router import router as session_router
+from backend.api.users.router import router as user_router
+from backend.core.config import settings
 
 
 @asynccontextmanager
@@ -25,5 +25,4 @@ app.include_router(auth_router, prefix=f"{settings.API_V1_STR}/auth", tags=["aut
 app.include_router(queue_router, prefix=f"{settings.API_V1_STR}/queue", tags=["queue"])
 app.include_router(search_router, prefix=f"{settings.API_V1_STR}/search", tags=["search"])
 app.include_router(session_router, prefix=f"{settings.API_V1_STR}/session", tags=["session"])
-app.include_router(user_router, prefix=f"{settings.API_V1_STR}/user", tags=["user"])
-
+app.include_router(user_router, prefix=f"{settings.API_V1_STR}/users", tags=["users"])
