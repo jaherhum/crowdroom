021257908c35505cd4ac596f55b73dfa7897cf8e
José Antonio Hernández Humanes <hello@jaherhum.dev>
2026-04-16 23:25:15 +0200

    refactor(core): update exceptions and main entry point for auth

--- backend/core/exceptions.py
+++ backend/core/exceptions.py
@@ -2,7 +2,8 @@ from uuid import UUID
 
 
 class AppException(Exception):
-    """Base class for all application-specific exceptions.
+    """
+    Base class for all application-specific exceptions.
 
     Attributes:
         message (str): The error message.
@@ -12,7 +13,8 @@ class AppException(Exception):
 
 
 class EntityNotFoundException(AppException):
-    """Exception raised when a requested entity is not found in the database.
+    """
+    Exception raised when a requested entity is not found in the database.
 
     Attributes:
         entity_name (str): The name of the entity type (e.g., 'User').
@@ -23,3 +25,27 @@ class EntityNotFoundException(AppException):
         self.entity_name = entity_name
         self.entity_id = entity_id
         super().__init__(f"Entity {entity_name} with ID {entity_id} not found")
+
+class EntityExistsException(AppException):
+    """
+    Exception raised when a requested entity already exists in the database.
+    Attributes:
+        entity_name (str): The name of the entity type (e.g., 'User').
+    """
+    def __init__(self, entity_name: str):
+        self.entity_name = entity_name
+        super().__init__(f"Entity {entity_name} already exists")
+
+class EntityDoesNotExistException(AppException):
+    """
+    Exception raised when a requested entity does not exist in the database.
+    Attributes:
+        entity_name (str): The name of the entity type (e.g., 'User').
+    """
+    def __init__(self, entity_name: str):
+        self.entity_name = entity_name
+        super().__init__(f"Entity {entity_name} does not exist")
+
+class InvalidCredentialsException(AppException):
+    def __init__(self):
+        super().__init__(f"Invalid username/email or password.")
