336e9314dce9ab29201baeb3fe4687d9afcfec4d
José Antonio Hernández Humanes <189163128+jaherhum@users.noreply.github.com>
2026-04-20 23:08:55 +0200

    refactor: improve model imports and add missing type hints

---
+++ backend/db/models/__init__.py
@@ -0,0 +1,6 @@
+from backend.db.models.enum import StreamingPlatforms, PlaybackStatus, TokenType
+from .user import User
+from .room import Room
+from .song import Song
+from .session import Session
+from .queue_item import QueueItem
