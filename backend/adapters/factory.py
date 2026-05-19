9aaf75ca91b51f1aaf05932dc08be99a283ab3de
José Antonio Hernández Humanes <hello@jaherhum.dev>
2026-05-19 23:05:26 +0200

    fix(imports): change relative to absolute imports

--- backend/adapters/factory.py
+++ backend/adapters/factory.py
@@ -1,6 +1,6 @@
-from adapters.base import BaseAdapter
-from adapters.spotify_search_adapter import SpotifySearchAdapter
-from db.models import StreamingPlatforms
+from backend.adapters.base import BaseAdapter
+from backend.adapters.spotify_search_adapter import SpotifySearchAdapter
+from backend.db.models import StreamingPlatforms
 
 
 class AdapterFactory:
